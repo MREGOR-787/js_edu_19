88fc3fba2e5a335297900101a278fefe6dca8161
MREGOR-787 <38703434+MREGOR-787@users.noreply.github.com>
2019-04-27 20:39:56 +0300

    Layout перенесён во views

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,16 @@
 <template>
   <div>
-    <h1>Наше приложение</h1>
-    <nav>
-      <ul>
-        <li>
-          <router-link to="/">Новости</router-link>
-        </li>
-        <li>
-          <router-link to="/chat">Чат</router-link>
-        </li>
-        <li>
-          <router-link to="/store">Магазин</router-link>
-        </li>
-      </ul>
-    </nav>
-    <router-view></router-view>
+    <layout></layout>
   </div>
 </template>
 
 <script>
+import Layout from "@/views/Layout";
 export default {
   name: "App",
-  components: {},
+  components: { Layout },
   data: () => ({
-    page: "news"
+    page: "chat"
   })
 };
 </script>
