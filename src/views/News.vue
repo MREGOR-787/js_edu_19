4f73fe2515c9265e3a9528f33c3011d8766aeb17
MREGOR-787 <38703434+MREGOR-787@users.noreply.github.com>
2019-04-28 13:33:08 +0300

    Переход на фреймворк, страница новостей

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,11 +1,48 @@
 <template>
   <div>
     <h2>Новости</h2>
+    <v-card v-for="(item, key) in news" :key="key" style="margin:20px">
+      <v-img :src="item.img"></v-img>
+
+      <v-card-title primary-title>
+        <div>
+          <h3 class="headline mb-0">{{ item.title }}</h3>
+          <div>{{ item.text }}</div>
+        </div>
+      </v-card-title>
+
+      <v-card-action>
+        <v-btn flat color="orange">Share</v-btn>
+        <v-btn flat color="orange">Explore</v-btn>
+      </v-card-action>
+    </v-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: "News"
+  name: "News",
+  data: () => ({
+    news: [
+      {
+        title: "Заголовок",
+        text:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, aspernatur repudiandae molestias iure blanditiis dolor earum saepe? Ad officia similique possimus unde dolorum dolores reiciendis dolorem. Totam ullam iusto dolorum!",
+        img: ""
+      },
+      {
+        title: "Заголовок",
+        text:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, aspernatur repudiandae molestias iure blanditiis dolor earum saepe? Ad officia similique possimus unde dolorum dolores reiciendis dolorem. Totam ullam iusto dolorum!",
+        img: ""
+      },
+      {
+        title: "Заголовок",
+        text:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, aspernatur repudiandae molestias iure blanditiis dolor earum saepe? Ad officia similique possimus unde dolorum dolores reiciendis dolorem. Totam ullam iusto dolorum!",
+        img: ""
+      }
+    ]
+  })
 };
 </script>
